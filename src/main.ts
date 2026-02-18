c60ab70de9e13dd5e115191dc93e847a12fedb11
Conor Sullivan <conor@conorsullivan.net>
2026-02-17 23:44:40 -0600

    added member comping and member search by email

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken');
 const baseURL = "localhost:2368";
 const adminEndpoint = "ghost/api/admin";
 
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -49,6 +48,51 @@ async function addMember(token:string, email:string) {
   })
 }
 
+async function getMemberByEmail(token:string, email:string):Promise<any> {
+  const res = await fetch(`http://${baseURL}/${adminEndpoint}/members?search=${email}`, {
+    "headers": {
+      "Accept-Version": "v6.0",
+      "Authorization": `Ghost ${token}`,
+      "Content-Type": "application/json"
+    },    
+  })
+  let result:any = await res.json()
+  console.log(result.members[0])
+  return result.members[0]
+  
+}
+
+//This is how members are comped:
+async function upgradeMember(token:string, email:string) {
+/**PUT something like this:
+ * {"members":[{"id":"6992970f3d2cd3305af074fd","email":"foo@example.com","tiers":[{"id":"698d571e8aff06d0de0020af","expiry_at":"2026-03-18T00:00:00.000Z"}]}]}
+ */
+  let member = await getMemberByEmail(token, email);
+  let body = {
+    "members": [{id:member.id, email: member.email,
+    "tiers": [
+      {
+        "id": "698d571e8aff06d0de0020af", //hardcoded tier derived from test ghost instance
+        "expiry_at": new Date('2026-12-31').toISOString()
+      }
+    ]}]
+  }
+
+  //I think member.id is required in the URL even though we're passing in an array of members
+  const res = await fetch(`http://${baseURL}/${adminEndpoint}/members/${member.id}/?include=tier`, {
+    "method": "PUT",
+    "headers": {
+      "Accept-Version": "v6.18",
+      "Authorization": `Ghost ${token}`,
+      "Content-Type": "application/json"
+    },
+    "body": JSON.stringify(body)
+  })
+  res.json().then((data:any) => {
+    console.log(data);
+  })
+}
+
 function main(){
   const token:string = jwt.sign({},Buffer.from(process.env.ADMIN_SECRET!, 'hex'),
   {
@@ -58,7 +102,7 @@ function main(){
     audience: "/admin/"
   });
   rl.question(
-    "What would you like to do? (options: getmembers, addmember, quit): ",
+    "What would you like to do? (options: getmembers, getmember, addmember, addpaid, quit): ",
     (command: string) => {
       if (command === "getmembers") {
         listMembers(token);
@@ -69,6 +113,15 @@ function main(){
       } else if (command === "quit") {
         console.log("Good Bye!");
         process.exit(0);
+      }
+      else if (command === "addpaid"){
+        rl.question("Please enter the lucky member's email: ", (email: string) => {
+          upgradeMember(token,email)
+        })
+      } else if (command === "getmember"){
+        rl.question("Please enter the member's email: ", (email: string) => {
+          getMemberByEmail(token, email);
+        })
       } else {
         console.log("Idk that command.");
         process.exit(1);
